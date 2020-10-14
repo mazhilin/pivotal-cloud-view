20948f9268e00c7aa68622f64e1156e0b90f12bb
marklin <marklin1992@outlook.com>
2020-10-14 22:01:53 +0800

    master[2020-09-30 星期三 17:48]:commit to master--项目主题更新等

---
+++ pivotal-passport-view/public/js/global.js
@@ -0,0 +1,9 @@
+/**引入定义全局component组件包加载相关js*/
+document.writeln('<script charset="utf-8" rel="application/javascript" src="../../component/vue-2.6.10/dist/vue.min.js"></script>');
+document.writeln('<script charset="utf-8" rel="application/javascript" src="../../component/vue-router-3.1.3/dist/vue-router.min.js"></script>');
+document.writeln('<script charset="utf-8" rel="application/javascript" src="../../component/vuex-3.1.2/dist/vuex.min.js"></script>');
+document.writeln('<script charset="utf-8" rel="application/javascript" src="../../component/axios-0.19.0/dist/axios.min.js"></script>');
+document.writeln('<script charset="utf-8" rel="application/javascript" src="../../component/element-2.12.0/dist/element-ui.js"></script>');
+document.writeln('<script charset="utf-8" rel="application/javascript" src="../../component/iconfont-1.0.0/dist/iconfont.js"></script>');
+/**引入自定义封装组件js*/
+document.writeln('<script charset="utf-8" rel="application/javascript" src="../../public/js/router.js"></script>');
